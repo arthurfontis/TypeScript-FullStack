7dfce7057ec8cf9c1442d38c9eacf288484ac3a7
ArthurFontis <arthurfontis17@gmail.com>
2026-04-12 11:24:37 -0300

    12/04/26

--- fundamentos/app.ts
+++ fundamentos/app.ts
@@ -1,28 +1,90 @@
-interface People {
-    name: string,
-    age: number,
-    profission?:string
-}
+//AULA - ARRAY MANIPULANDO
+const array: Array<number> = [1,2,3,4]
+const buscaNum = array.find(num => num > 2) //retorna o primeiro valor que achar na condição
 
-const people: People = {
-    name: "Fontis",
-    age: 19
-}
+array.forEach( num => {
+    if(num % 2 == 0){
+        console.log(num)
+    }
+})
+console.log('-------------------')
 
-const outerPeople: People = {
-    name: 'Raimundo',
-    age: 50,
-    profission: 'Pedreiro'
-}
+array.map(num => {
+    if(num > 2 ){
+        console.log(num)
+    }
+})
 
-const arrayPeople: People[] = [  //Array<People>, dizendo que o array é do tipo obj(People)
-    people,
-    outerPeople
-]
+console.log('-------------------')
+console.log(buscaNum)
 
-const arrayNum: number[] = [
-    1,2,3
-]
-const arrayStr: Array<string> = [
-    'a','b','c'
-]
+//AULA - ARRAY  
+// const array: Array<number> = [1,2,3,4,5]
+
+// const stringArray: string[] = ['a','b','c']
+
+// console.log(array)
+// console.log(stringArray.length)
+
+// array.push(6)
+// console.log(array)
+// array.pop()
+// console.log(array)
+
+
+//AULA - DECISÕES E REPETIÇÕES
+// const num: number = 15
+// if(num > 15){
+//     console.log("número maior que 15")
+// } else if(num === 15) {
+//     console.log("número igual a 15")
+// } else {
+//     console.log("Número menor que 15")
+// }
+
+// const typeuser = {
+//     adm: "Seja Bem vindo ADM",
+//     student: "Seja Bem vindo student",
+//     viewer: "Você pode vizualizar apenas"
+// }
+
+
+// function validateUser(user: string){
+//     console.log(typeuser[user as keyof typeof typeuser])
+// }
+// const user = 'adm'
+
+// validateUser(user)
+// validateUser('student')
+// validateUser('viewer')
+
+
+//AULA - OBJETOS E INTERFACES
+// interface People {
+//     name: string,
+//     age: number,
+//     profission?:string
+// }
+
+// const people: People = {
+//     name: "Fontis",
+//     age: 19
+// }
+
+// const outerPeople: People = {
+//     name: 'Raimundo',
+//     age: 50,
+//     profission: 'Pedreiro'
+// }
+
+// const arrayPeople: People[] = [  //Array<People>, dizendo que o array é do tipo obj(People)
+//     people,
+//     outerPeople
+// ]
+
+// const arrayNum: number[] = [
+//     1,2,3
+// ]
+// const arrayStr: Array<string> = [
+//     'a','b','c'
+// ]
